8fe68b032ee8c0db107165a54ce95072257f0543
Marek Kubica <marek@xivilization.net>
2015-02-07 22:22:28 +0100

    Add lein-exec for easier execution of scripts

--- home/.lein/profiles.clj
+++ home/.lein/profiles.clj
@@ -1,3 +1,4 @@
-{:user {:plugins [[lein-kibit "0.0.8"]]
+{:user {:plugins [[lein-kibit "0.0.8"]
+                  [lein-exec "0.3.4"]]
         :dependencies [[slamhound "1.3.3"]]
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
