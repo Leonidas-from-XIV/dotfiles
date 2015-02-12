b2293e5c34f3d41f34fc80372016ae49123544fb
Marek Kubica <marek@xivilization.net>
2015-02-13 00:29:54 +0100

    Added ultra leiningen plugin

--- home/.lein/profiles.clj
+++ home/.lein/profiles.clj
@@ -1,4 +1,6 @@
 {:user {:plugins [[lein-kibit "0.0.8"]
-                  [lein-exec "0.3.4"]]
+                  [lein-exec "0.3.4"]
+                  [venantius/ultra "0.2.0"]]
         :dependencies [[slamhound "1.3.3"]]
+        :ultra {:color-scheme :solarized_dark}
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
