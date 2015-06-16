bc0ba429008fdaf6dc84c9b3f11547f40c6baf85
Marek Kubica <marek.kubica@stylefruits.de>
2015-06-16 09:40:39 +0200

    Update Clojure dependencies in Leiningen

--- home/.lein/profiles.clj
+++ home/.lein/profiles.clj
@@ -1,7 +1,9 @@
-{:user {:plugins [[lein-kibit "0.0.8"]
-                  [lein-exec "0.3.4"]
-                  [venantius/ultra "0.2.0"]
-                  [cider/cider-nrepl "0.8.2"]]
-        :dependencies [[slamhound "1.3.3"]]
+{:user {:plugins [[lein-kibit "0.1.2"]
+                  [lein-exec "0.3.5"]
+                  [venantius/ultra "0.3.3"]
+                  [cider/cider-nrepl "0.8.2"]
+                  [lein-ancient "0.6.7"]]
+        :dependencies [[slamhound "1.5.5"]
+                       [pr-foobar "0.1.1"]]
         :ultra {:color-scheme :solarized_dark}
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
