6bd86424be0581f470d740773e8789ef650c2876
Marek Kubica <marek@xivilization.net>
2015-06-22 09:03:24 +0200

    Add nrepl support for CIDER

--- home/.lein/profiles.clj
+++ home/.lein/profiles.clj
@@ -2,8 +2,11 @@
                   [lein-exec "0.3.5"]
                   [venantius/ultra "0.3.3"]
                   [cider/cider-nrepl "0.8.2"]
+                  [refactor-nrepl "0.3.0-SNAPSHOT"]
                   [lein-ancient "0.6.7"]]
-        :dependencies [[slamhound "1.5.5"]
+        :dependencies [[alembic "0.3.2"]
+                       [org.clojure/tools.nrepl "0.2.7"]
+                       [slamhound "1.5.5"]
                        [pr-foobar "0.1.1"]]
         :ultra {:color-scheme :solarized_dark}
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
