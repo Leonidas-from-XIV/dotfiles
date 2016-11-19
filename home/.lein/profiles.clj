bc22a94b134d7815fa0f14186101b20892f2d6f9
Marek Kubica <marek@xivilization.net>
2016-11-19 18:08:39 +0100

    Use newer lein-ancient

--- home/.lein/profiles.clj
+++ home/.lein/profiles.clj
@@ -4,7 +4,7 @@
                        [org.clojure/tools.nrepl "0.2.12" :exclusions [org.clojure/clojure]]]}
  :user {:plugins [[lein-kibit "0.1.2" :exclusions [org.clojure/clojure]]
                   [lein-exec "0.3.5"]
-                  [lein-ancient "0.6.7" :exclusions [org.clojure/clojure]]]
+                  [lein-ancient "0.6.10" :exclusions [org.clojure/clojure]]]
         :dependencies [[slamhound "1.5.5"]
                        [pr-foobar "0.1.1"]]
         :ultra {:color-scheme :solarized_dark}
