eb3a501a601a71552a7841b147d09913f63a0183
Marek Kubica <marek@xivilization.net>
2021-02-17 13:09:23 +0100

    Update .gitconfig for new projects

--- home/.lein/profiles.clj
+++ home/.lein/profiles.clj
@@ -1,11 +1,4 @@
-{:repl {:plugins [[cider/cider-nrepl "0.11.0-SNAPSHOT"]
-                  [refactor-nrepl "2.0.0-SNAPSHOT"]]
-        :dependencies [[alembic "0.3.2"]
-                       [org.clojure/tools.nrepl "0.2.12" :exclusions [org.clojure/clojure]]]}
- :user {:plugins [[lein-kibit "0.1.2" :exclusions [org.clojure/clojure]]
-                  [lein-exec "0.3.5"]
-                  [lein-ancient "0.6.10" :exclusions [org.clojure/clojure]]]
-        :dependencies [[slamhound "1.5.5"]
-                       [pr-foobar "0.1.1"]]
-        :ultra {:color-scheme :solarized_dark}
-        :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
+{:user {:plugins [[cider/cider-nrepl "0.25.6"]
+                  [lein-kibit "0.1.8" :exclusions [org.clojure/clojure]]]
+        :dependencies [[hashp "0.2.0"]]
+        :injections [(require 'hashp.core)]}}
